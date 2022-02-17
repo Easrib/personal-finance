e5cfb91e269ad445811b07bced5d5fc0a03c3198
Easrib <easrib.sharik01@gmail.com>
2022-02-17 10:20:51 +0600

    saving-expense-error-handle

--- js/app.js
+++ js/app.js
@@ -13,20 +13,36 @@ const getExpense = function () {
 
     return totalExpense;
 };
+let errorMessage = document.getElementById('error-message');
 const calculate = document.getElementById('calculate-btn').addEventListener('click', function () {
-    const ExpenseCalc = document.getElementById('expense-calc');
-    ExpenseCalc.innerText = getExpense();
-    const balanceCalc = document.getElementById('balance-calc');
-    balanceCalc.innerText = getIncome() - getExpense();
+    if (getIncome() >= getExpense()) {
+        const ExpenseCalc = document.getElementById('expense-calc');
+        ExpenseCalc.innerText = getExpense();
+        const balanceCalc = document.getElementById('balance-calc');
+        balanceCalc.innerText = getIncome() - getExpense();
+        errorMessage.style.display = 'none';
+    }
+    else {
+        errorMessage.style.display = 'block';
+    }
 });
-
+let errorSavingMessage = document.getElementById('saving-error');
 const savings = document.getElementById('save-btn').addEventListener('click', function () {
     let savePercantageText = document.getElementById('save-input');
     let savePercantage = parseFloat(savePercantageText.value);
     let savingAmount = document.getElementById("saving-amount");
-    savingAmount.innerText = getIncome() * (savePercantage / 100);
     let remainingBalance = document.getElementById('remaining-amount');
-    remainingBalance.innerText = getIncome() - getExpense() - parseFloat(savingAmount.innerText);
+    let savingCheck = getIncome() * (savePercantage / 100);
+    if ((getIncome() - getExpense()) > savingCheck) {
+        savingAmount.innerText = savingCheck;
+        remainingBalance.innerText = getIncome() - getExpense() - parseFloat(savingAmount.innerText);
+        errorSavingMessage.style.display = 'none';
+    }
+    else {
+        errorSavingMessage.style.display = 'block';
+        savingAmount.innerText = 0;
+        remainingBalance.innerText = 0;
+    }
 })
 
 // financial calculation ends
